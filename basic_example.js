df1fe7db87de81b32c6b8302299156aa1524b832
sheoransumit <op.sumitsheoran@gmail.com>
2016-11-23 15:29:59 +0530

    Update basic_example.js

--- basic_example.js
+++ basic_example.js
@@ -11,19 +11,21 @@ Companies.findOneAndUpdate(query, req.newData, {upsert:true}, function(err, doc)
     return res.send("succesfully saved");
 });
 
+// Insert These two for Instance Method to work
+// var xyz = new Vehicle({ numberplate: 'MH02CB1234', models:{company:'Maruti', category:'Sedan',models[model: }});
+// var abc = new Vehicle({ numberplate: 'MH02CB1212', models:{company:'Honda', category:'Sedan',models[model: }});
+
 // Then generate pricelist(model specific) and print a bill with total amount as backend calculation.
 
-// Search vehicle by numberplate
+// Search vehicle by numberplate(Static Method)
 var Vehicle = mongoose.model('Vehicle', vehicleSchema);
 Vehicle.findByNumberPlate('MH02CB1212', function(err, vehicles) {
   console.log(vehicles);
 });
 
-// Query to get all numberplates with specific model and store it in modelvehicles schema
+// Query to get all numberplates with specific model and store it in modelvehicles schema(Instance Method)
 // var Vehicles = require('./vehicle');
 var Vehicle = mongoose.model('Vehicle', vehicleSchema);
-var xyz = new Vehicle({ numberplate: 'MH02CB1234', models:{company:'Maruti', category:'Sedan',models[model: }});
-var abc = new Vehicle({ numberplate: 'MH02CB1212', models:{company:'Honda', category:'Sedan',models[model: }});
 var def = new Vehicle({ numberplate: 'MH02CB1111', models:{company:'Maruti', category:'Hatchback',models[model: }});
 
 xyz.findSimilarCompany(function(err, xyz) {
