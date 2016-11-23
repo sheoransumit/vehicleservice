3e35f550fca4d760e8fdb1b9db5c90f3c3300baa
sheoransumit <op.sumitsheoran@gmail.com>
2016-11-23 14:33:47 +0530

    Update vehicle.js

--- vehicle.js
+++ vehicle.js
@@ -19,5 +19,8 @@ var Vehicle = mongoose.model('Vehicle', vehicleSchema);
 vehicleSchema.methods.findSameCompany = function(cb) {
   return this.model('Vehicle').find({ models.company: this.models.company }, cb);
 };
+vehicleSchema.methods.findSameCompanyandCategory = function(cb, de) {
+  return this.model('Vehicle').find({ models.company: this.models.company, models.category: this.models.category }, cb, de);
+};
 // module.exports = new mongoose.Schema(vehicleSchema);
 // module.exports.vehicleSchema = vehicleSchema;
